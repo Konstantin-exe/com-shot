3a0c58ed00163dad11b384e59e2811da57d85a2e
Konstantin Müllner <konstantinmullner@MacBook-Air-von-Konstantin.local>
2021-07-31 20:05:49 +0200

    broken build - terminal opens, write text in terminal working on

--- src/extension.ts
+++ src/extension.ts
@@ -3,24 +3,41 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // process.stdout.write('Hello world!');
   console.log('Hello from console log');
+
+    /*
+    todo: declare type or find callback for sendCommitMessage
+    */
+
+
+  interface TTerminal {
+    vscode.window.activeTerminal: Boolean,
+  }
 
   let disposable = vscode.commands.registerCommand(
     'com-shot.saveCommit',
     () => {
       let createNewTerminal = vscode.window.createTerminal({
         name: 'My Command',
+        // todo: set current branch for cwd. cwd: "pathToCurrentBranch"
         hideFromUser: false,
-        message: 'hello world',
-        shellArgs: 'hello world',
       });
       createNewTerminal.show();
 
-      vscode.window.showInformationMessage(
-        'Your Commit-SnapShot has being saved',
-      );
+      let sendCommitMessage =
+      if (vscode.window.activeTerminal === true) {
+      vscode.window.activeTerminal
+        ? {
+            name: 'My Command',
+            // sendText: 'hello world',
+          }
+        : ;
+      sendCommitMessage.sendText('he');
     },
+  );
+
+  vscode.window.showInformationMessage(
+    'Your Commit-SnapShot has being saved',
   );
 
   context.subscriptions.push(disposable);
